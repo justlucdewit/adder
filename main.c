ffeb5465377ae6cccfb16b36638800e67ef1fce6
Luc de wit <luc.dewit@hotmail.nl>
2020-01-25 12:06:23 +0100

    stopping cuz the tears are to much

--- main.c
+++ main.c
@@ -1,4 +1,16 @@
+/*
+	code is written by luc de wit
+	project started on tueseday 23 december 2020
+	if you are going to work on this, i wish you the best of luck
+	and hope you can keep up your mentality, sorry for the trouble :(
+	version works like this:
+	mainversion.mainupdate.minorupdate:part
+*/
+
 #include <stdio.h>
+#include <stdint.h>
+#include <stddef.h>
+#include <stdarg.h>
 #include <stdlib.h>
 #include <string.h>
 
@@ -9,76 +21,80 @@
 #define ANSIGREEN "\033[92m"
 #define ANSISTOP "\033[0m"
 
-//version works like this:
-//mainversion.mainupdate.minorupdate:part
-
 //project information
 #define EXTENSION ".add"
 #define VERSION "adder 1.1.1:2"
 #define NAME "adder"
 
-char* debugIndicator = "\033[92m[Debug]";
-char* errorIndicator = "\033[91m[Error]";
+//! ansi colors dont work in cmd :(
+char *debugIndicator = "\033[92m[Debug]";
+char *errorIndicator = "\033[91m[Error]";
 
 //boolean to for debugging mode
 int debug = 1;
 
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
 	//error if no arguments were supplied
-	if (argc < 2){
+	if (argc < 2)
+	{
 		printf("%s it seems like you didnt supply enough arguments, try \"%s help\"", errorIndicator, NAME);
 		printf("\n");
-		printf("\033[0m");//stop ansi colors
-		return 1;
+		printf("\033[0m"); //stop ansi colors
+		return 0;
 	}
 
 	//run command
-	if (strcmp(argv[1], "run") == 0){
+	if (strcmp(argv[1], "run") == 0)
+	{
 		//check if a file name has been given
-		if (argc < 3){
+		if (argc < 3)
+		{
 			printf("%s it seems like you tried running a file but didnt give a filename, try \"%s run path/to/file\"", errorIndicator, NAME);
 			printf("\n");
-			printf("\033[0m");//stop ansi colors
-			return 1;
+			printf("\033[0m"); //stop ansi colors
+			return 0;
 		}
-		
+
 		//concatinate and store the file name with extension
-		char* fileName = malloc(strlen(argv[2]) + strlen(EXTENSION) + 1);
+		char *fileName = malloc(strlen(argv[2]) + strlen(EXTENSION) + 1);
 		strcpy(fileName, argv[2]);
 		strcat(fileName, EXTENSION);
 
 		//extract content from file
-		char* fileContent = malloc(0);
-		retrieveContent(fileContent, fileName, debug, debugIndicator, errorIndicator);
+		retrieveContent(fileName, debug, debugIndicator, errorIndicator);
+		//printf("filecontent: %s\n", fileContent);
 
 		printf("\n");
-		printf("\033[0m");//stop ansi colors
+		printf("\033[0m"); //stop ansi colors
 		return 0;
 	}
 
 	//help command
-	else if (strcmp(argv[1], "help") == 0){
+	else if (strcmp(argv[1], "help") == 0)
+	{
 		printf("%s help\t\tdisplays all the available commands for adder\n", NAME);
 		printf("%s version\t\tdisplays the currently installed version of adder\n", NAME);
 		printf("%s run [filename]\truns the program giving as argument\n", NAME);
-		printf("\033[0m");//stop ansi colors
+		printf("\033[0m"); //stop ansi colors
 		return 0;
 	}
 
 	//version command
-	else if (strcmp(argv[1], "version") == 0){
+	else if (strcmp(argv[1], "version") == 0)
+	{
 		printf(VERSION);
 		printf("\n");
-		printf("\033[0m");//stop ansi colors
+		printf("\033[0m"); //stop ansi colors
 		return 0;
 	}
 
 	//invallid command
-	else{
+	else
+	{
 		printf("%s it seems like %s is a invalid command, try \"%s help\"", errorIndicator, argv[1], NAME);
 		printf("\n");
-		printf("\033[0m");//stop ansi colors
+		printf("\033[0m"); //stop ansi colors
 		return 0;
 	}
 }
--- retrieveContent.h
+++ retrieveContent.h
@@ -1,24 +1,38 @@
-int retrieveContent(char* output, char* url, int debug, char* debugIndicator, char* errorIndicator){
-    if (debug == 1){//debug reading a file
-	    printf("%s reading from file: \"%s\"\n", debugIndicator, url);
-	}
+//todo make the function return a string instead of modifying one
+//todo make the function actually work
+void retrieveContent(char *url, int debug, char *debugIndicator, char *errorIndicator)
+{
+    if (debug == 1)
+    { //debug reading a file
+        printf("%s reading from file: \"%s\"\n", debugIndicator, url);
+    }
 
     //create file pointer
-    FILE* fp;
-    char str[1000];
-    fp = fopen(url, "r");
+    FILE *fp = fopen(url, "r");
 
     //error if file doest exist
-    if (fp == NULL){
+    if (fp == NULL)
+    {
         printf("%s it seems like the file you are trying to run (%s) doesnt exist", errorIndicator, url);
-        printf("\033[0m");//stop ansi colors
+        printf("\033[0m"); //stop ansi colors
         exit(0);
     }
 
-    while (fgets(str, 1000, fp) != NULL){
-        printf("%s reading line: %s", debugIndicator, str);
+    int n = 0;
+    int size = 0;
+    while (fscanf(fp, "%*[^\n]\n%n", &n) != EOF)
+    {
+        size += n;
+        printf("%d\n", size);
     }
 
+    //printf("%d\n", size);
+    char *output = malloc(size + 2);
+
+    //retrieve file content
+    fread(output, 1, size, fp);
+    //output[strlen(output)] = '\0';
+    printf("%s\n", output);
+
     fclose(fp);
-    return strlen(output);//this is just to make the compiler happy
 }
