dcda742164c0644926841d747f74405a2ac3dd8f
Luc de wit <luc.dewit@hotmail.nl>
2020-01-26 22:31:42 +0100

    fixed printing

--- printcolor.h
+++ printcolor.h
@@ -9,21 +9,21 @@ void printcolor(char *formatString, char *color, ...)
     va_list args;
     va_start(args, color);
 
-    char *coloredformat = "\033[37m[FEROR]";
-
     if (strcmp(color, "red") == 0)
     {
-        coloredformat = "\033[30m[Error]";
+        printf("\033[30m[Error]");
     }
 
     else if (strcmp(color, "green") == 0)
     {
-        coloredformat = "\033[32m[Debug]";
+        printf("\033[32m[Debug]");
     }
 
-    strcat(coloredformat, formatString);
-    strcat(coloredformat, "\033[0m");
-    vprintf(formatString, args);
+    printf("%s", formatString);
+
+    va_end(args);
+    fflush(stdout);
+    printf("\033[0m");
 }
 
 #endif
